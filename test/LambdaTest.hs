a293ef6fa61b7a7c626e0fd6c6b1f578c16692c7
odwl <odewolf@gmail.com>
2026-02-18 07:46:19 +0100

    test suite.

--- test/LambdaTest.hs
+++ test/LambdaTest.hs
@@ -6,10 +6,16 @@ import System.Exit (exitFailure, exitSuccess)
 test2 :: Test 
 test2 = TestCase (assertEqual "should square 2" 4 ((\a -> a^2) 2))
 
+testSquare :: Test 
+testSquare = TestCase (assertEqual "should give 16 " 16 ((\f -> (\x -> f (f x))) (\a -> a^2) 2))
+
+tests :: Test
+tests = TestList [test2, testSquare]
+
 main :: IO ()
 main = do
     putStrLn "\n--- Running: Lambda Suite ---"
-    counts <- runTestTT test2
+    counts <- runTestTT tests
     if errors counts + failures counts == 0
         then exitSuccess
         else exitFailure
