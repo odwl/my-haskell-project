bb2d4dc7b114a1ae0369b4c63828f57cf61a0201
odwl <odewolf@gmail.com>
2026-02-21 18:02:51 +0100

    style: reformat test definitions and data types in PangramTest.hs for improved readability.

--- test/Exercism/PangramTest.hs
+++ test/Exercism/PangramTest.hs
@@ -1,59 +1,65 @@
 {-# LANGUAGE RecordWildCards #-}
+
 module Exercism.PangramTest (pangramTests) where
 
-import Exercism.Pangram (isPangram)
-import Test.Tasty ( TestTree, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
-import Test.Tasty.QuickCheck
-    ( Property,
-      elements,
-      listOf,
-      shuffle,
-      suchThat,
-      (===),
-      counterexample,
-      forAll,
-      testProperty,
-      Arbitrary(arbitrary),
-      Gen )
+import Control.Monad (join)
 import Data.Char (toLower, toUpper)
 import Data.List (delete)
-import Control.Monad (join)
+import Exercism.Pangram (isPangram)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.QuickCheck
+  ( Arbitrary (arbitrary),
+    Gen,
+    Property,
+    counterexample,
+    elements,
+    forAll,
+    listOf,
+    shuffle,
+    suchThat,
+    testProperty,
+    (===),
+  )
 
 pangramTests :: TestTree
-pangramTests = testGroup "Pangram"
-    [ quickCheckPangramTests
-    , exampleTests
+pangramTests =
+  testGroup
+    "Pangram"
+    [ quickCheckPangramTests,
+      exampleTests
     ]
-    
+
 quickCheckPangramTests :: TestTree
-quickCheckPangramTests = testGroup "QuickCheck"
-    [ testProperty "pangram remains pangram when prepending any string" prop_prependPangram
-    , testProperty "empty string is never a pangram" $ not (isPangram "")
-    , testProperty "missing letter is not a pangram" prop_missingLetterIsNotPangram
+quickCheckPangramTests =
+  testGroup
+    "QuickCheck"
+    [ testProperty "pangram remains pangram when prepending any string" prop_prependPangram,
+      testProperty "empty string is never a pangram" $ not (isPangram ""),
+      testProperty "missing letter is not a pangram" prop_missingLetterIsNotPangram
     ]
 
 prop_prependPangram :: String -> Property
-prop_prependPangram s = 
-    let alphabet = "abcdefghijklmnopqrstuvwxyz"
-    in isPangram (s ++ alphabet) === True
+prop_prependPangram s =
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"
+   in isPangram (s ++ alphabet) === True
 
 -- This generator builds strings that are guaranteed to be missing one specific letter and no others.
 -- It includes uppercase, lowercase, numbers, unicodes and punctuation!
-genMissingLetterString :: Gen (Char, String) 
+genMissingLetterString :: Gen (Char, String)
 genMissingLetterString = do
-    (banned_lower, toBeUsed) <- genBannedLetterAndRemainingAlphabet
-    let safeCharGen = suchThat arbitrary ((/= banned_lower) . toLower)
-    noise <- listOf safeCharGen
-    shuffledString <- shuffle (noise ++ toBeUsed)
-    pure (banned_lower, shuffledString)
+  (banned_lower, toBeUsed) <- genBannedLetterAndRemainingAlphabet
+  let safeCharGen = suchThat arbitrary ((/= banned_lower) . toLower)
+  noise <- listOf safeCharGen
+  shuffledString <- shuffle (noise ++ toBeUsed)
+  pure (banned_lower, shuffledString)
 
 prop_missingLetterIsNotPangram :: Property
-prop_missingLetterIsNotPangram = 
-    forAll genMissingLetterString checkNotPangram
+prop_missingLetterIsNotPangram =
+  forAll genMissingLetterString checkNotPangram
   where
-    checkNotPangram (bannedChar, testString) = 
-        counterexample ("Banned letter: " ++ show bannedChar ++ "\nString: " ++ show testString) $
+    checkNotPangram (bannedChar, testString) =
+      counterexample ("Banned letter: " ++ show bannedChar ++ "\nString: " ++ show testString) $
         isPangram testString === False
 
 -- ==========================================
@@ -61,24 +67,28 @@ prop_missingLetterIsNotPangram =
 -- ==========================================
 
 exampleTests :: TestTree
-exampleTests = testGroup "Examples"
+exampleTests =
+  testGroup
+    "Examples"
     [ testCase explanation $ isPangram input @?= expected
-    | Case{..} <- cases
+      | Case {..} <- cases
     ]
 
-data Case = Case { explanation :: String, input :: String, expected :: Bool }
+data Case = Case {explanation :: String, input :: String, expected :: Bool}
+
 cases :: [Case]
-cases = [ Case "empty sentence" "" False
-        , Case "perfect lower case" "abcdefghijklmnopqrstuvwxyz" True
-        ]
+cases =
+  [ Case "empty sentence" "" False,
+    Case "perfect lower case" "abcdefghijklmnopqrstuvwxyz" True
+  ]
 
 -- Generates a tuple where the first element is a random lowercase letter (the "banned" letter),
 -- and the second element is a list containing all other letters of the alphabet in both
 -- lowercase and uppercase. This ensures the resulting list contains every letter except the banned one.
 genBannedLetterAndRemainingAlphabet :: Gen (Char, [Char])
 genBannedLetterAndRemainingAlphabet = do
-    let alphabet = ['a'..'z']
-    bannedLetter <- elements alphabet
-    let remainingLetters = delete bannedLetter alphabet
-    let remainingLettersBothCases = join ( fmap (\c -> [c, toUpper c]) remainingLetters)
-    pure (bannedLetter, remainingLettersBothCases)
+  let alphabet = ['a' .. 'z']
+  bannedLetter <- elements alphabet
+  let remainingLetters = delete bannedLetter alphabet
+  let remainingLettersBothCases = join (fmap (\c -> [c, toUpper c]) remainingLetters)
+  pure (bannedLetter, remainingLettersBothCases)
