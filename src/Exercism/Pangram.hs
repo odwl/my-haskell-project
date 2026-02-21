2d80bf8f0faba7db89f2a7f86dbed23d8795ff6f
odwl <odewolf@gmail.com>
2026-02-21 20:05:48 +0100

    Refactor Pangram: use literal implementation and remove magic number 26

--- src/Exercism/Pangram.hs
+++ src/Exercism/Pangram.hs
@@ -1,10 +1,9 @@
 module Exercism.Pangram (isPangram) where
 
-import Data.Char (isAlpha, isAscii, toLower)
-import Data.Set (fromList, size)
+import Data.Char (toLower)
 
 -- A pangram is a sentence using every letter of the alphabet at least once. It is case insensitive, so it doesn't matter if a letter is lower-case (e.g. k) or upper-case (e.g. K).
 -- For this exercise, a sentence is a pangram if it contains each of the 26 letters in the English alphabet.
 
 isPangram :: String -> Bool
-isPangram = (== 26) . size . fromList . map toLower . filter isAlpha . filter isAscii
+isPangram s = all (`elem` map toLower s) ['a' .. 'z']
