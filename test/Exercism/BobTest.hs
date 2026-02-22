f957a11a771a41f1da8f7722dda0083ab5cb94dd
odwl <odewolf@gmail.com>
2026-02-22 11:51:29 +0100

    Refactor BobTest QuickCheck generators
    Extract reusable generators (genAQuestion, genNotAQuestion, genNoise, genYelling) to improve property test readability and robustness.

--- test/Exercism/BobTest.hs
+++ test/Exercism/BobTest.hs
@@ -13,7 +13,6 @@ import Test.Tasty.QuickCheck
     elements,
     forAll,
     listOf,
-    listOf1,
     testProperty,
     (===),
   )
@@ -37,18 +36,24 @@ quickCheckBobTests =
     [ testProperty "silence always gets the same response" prop_silence,
       testProperty "yelling always gets a yelling response" prop_yelling,
       testProperty "questions get specific responses" prop_question_finished_by,
-      testProperty "not a question gets specific responses" prop_not_a_question
+      testProperty "not a question gets specific responses" prop_not_a_question,
+      testProperty "not silence gets specific responses" prop_not_silence
     ]
 
 prop_silence :: Property
 prop_silence =
-  forAll (listOf (elements " \t\n")) $ \s ->
-    hey s === responseString Fine
+  forAll genNoise $ \noise ->
+    hey noise === responseString Fine
+
+prop_not_silence :: Property
+prop_not_silence =
+  forAll genNotSilence $ \input ->
+    hey input /= responseString Fine
 
 prop_yelling :: Property
 prop_yelling =
-  forAll (listOf1 (elements ['A' .. 'Z'])) $ \s ->
-    hey s `elem` map responseString [Whoa, CalmDown]
+  forAll genYelling $ \input ->
+    hey input `elem` map responseString [Whoa, CalmDown]
 
 -- Verifies that any input ending with a question mark (followed by optional whitespace)
 -- is treated as a question by Bob.
@@ -58,21 +63,50 @@ prop_question_finished_by = forAll genAQuestion $ \input -> elem (hey input) $ m
 prop_not_a_question :: Property
 prop_not_a_question = forAll genNotAQuestion $ \input -> elem (hey input) $ map responseString [Whoa, Whatever]
 
+-- Example: "How are you?  "
 genAQuestion :: Gen String
 genAQuestion = do
   s <- arbitrary :: Gen String
   noise <- genNoise
   pure (s ++ "?" ++ noise)
 
+-- Example: "  \t"
 genNoise :: Gen String
 genNoise = listOf (elements (filter isSpace ['\0' .. '\127']))
 
+-- Example: "Hello!"
 genNotAQuestion :: Gen String
 genNotAQuestion = do
   s <- arbitrary :: Gen String
-  nonSpaceChar <- elements (filter (\c -> not (isSpace c) && c /= '?') ['\0' .. '\127'])
-  noise <- listOf (elements (filter isSpace ['\0' .. '\127']))
+  nonSpaceChar <- genNonSpaceChar
+  noise <- genNoise
   pure (s ++ [nonSpaceChar] ++ noise)
+
+-- Example: 'A'
+genNonSpaceChar :: Gen Char
+genNonSpaceChar = elements $ filter (\c -> not (isSpace c) && c /= '?') ['\0' .. '\127']
+
+-- Example: " a "
+genNotSilence :: Gen String
+genNotSilence = do
+  n1 <- genNoise
+  c <- genNonSpaceChar
+  n2 <- genNoise
+  pure (n1 ++ [c] ++ n2)
+
+-- Example: "1, 2, 3 GO!  "
+genYelling :: Gen String
+genYelling = do
+  -- Base noise (spaces) and punctuation/numbers
+  baseNoise <- listOf (elements (filter (\c -> isSpace c || (c >= '0' && c <= '9') || c `elem` ("!@#$%^&*,." :: String)) ['\0' .. '\127']))
+  -- We MUST have at least one uppercase letter
+  upperCaseLetter <- elements ['A' .. 'Z']
+  -- Optional additional uppercase letters
+  moreUpperCase <- listOf (elements ['A' .. 'Z'])
+
+  -- Shuffle everything or just append nicely? Let's spread it nicely.
+  -- To keep it simple but rigorous, we'll embed the uppercase letters inside the noise
+  pure (baseNoise ++ [upperCaseLetter] ++ moreUpperCase ++ baseNoise)
 
 -- ==========================================
 -- Example Tests
