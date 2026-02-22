e05c6cb2d79af15b3a02106f090f641d58990c35
odwl <odewolf@gmail.com>
2026-02-22 16:05:29 +0100

    refactor: reimplement `isPangram` to use `all` and `elem` for character presence, removing `Data.Set` and `isAscii` filter.

--- src/Exercism/Pangram.hs
+++ src/Exercism/Pangram.hs
@@ -1,10 +1,15 @@
 module Exercism.Pangram (isPangram) where
 
-import Data.Char (isAlpha, isAscii, toLower)
-import Data.Set (fromList, size)
+import Data.Char
 
 -- A pangram is a sentence using every letter of the alphabet at least once. It is case insensitive, so it doesn't matter if a letter is lower-case (e.g. k) or upper-case (e.g. K).
 -- For this exercise, a sentence is a pangram if it contains each of the 26 letters in the English alphabet.
 
 isPangram :: String -> Bool
-isPangram = (== length ['a' .. 'z']) . size . fromList . map toLower . filter isAlpha . filter isAscii
+isPangram text = all (flip elem lowerText) ['a' .. 'z']
+  where
+    lowerText = map toLower text
+
+-- import Data.Char (isAlpha, isAscii, toLower)
+-- import Data.Set (fromList, size)
+-- isPangram = (== length ['a' .. 'z']) . size . fromList . map toLower . filter isAlpha . filter isAscii
