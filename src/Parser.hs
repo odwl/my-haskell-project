cdba0e890122442193cccce602b0901f2641ec4a
odwl <odewolf@gmail.com>
2026-02-23 14:48:31 +0100

    refactor: remove commented-out parser examples and instructions

--- src/Parser.hs
+++ src/Parser.hs
@@ -28,18 +28,3 @@ endOfStream = Parser f
   where
     f [] = Just ((), "")
     f _ = Nothing
-
--- term1 c = Parser f
---   where
---     f [] = Nothing
---     f (x : xs)
---       | x == c = Just (x, xs)
---       | otherwise = Nothing
-
---       1. Write a constructor for a parser: term1
--- , which takes a Char and returns a Parser Char which will
--- successfully parse the first character of the input if it is the character in question and will fail otherwise.
--- 2. Write a constructor for a parser: digit which will parse one digit (0 − 9) from the input.
--- 3. Write a constructor for a parser singularWhiteSpace which will parse a whitespace character (consider
--- only the four whitespace characters: space, tab, carriage return, and linefeed) from the input.
--- 4. Write a constructor for a parser endOfStream: which succeeds if the string is empty and fails otherwise.
