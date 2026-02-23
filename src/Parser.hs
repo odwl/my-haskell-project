d0bbee06d6bc23a79fefe3a0a85adc0aec532bbf
odwl <odewolf@gmail.com>
2026-02-23 09:18:39 +0100

    Add term1 parser and QuickCheck properties

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,4 +1,4 @@
-module Parser (Parser (..), satisfy) where
+module Parser (Parser (..), satisfy, term1) where
 
 newtype Parser a = Parser
   { parse :: String -> Maybe (a, String)
@@ -11,3 +11,21 @@ satisfy p = Parser f
     f (x : xs)
       | p x = Just (x, xs)
       | otherwise = Nothing
+
+term1 :: Char -> Parser Char
+term1 c = satisfy (== c)
+
+-- term1 c = Parser f
+--   where
+--     f [] = Nothing
+--     f (x : xs)
+--       | x == c = Just (x, xs)
+--       | otherwise = Nothing
+
+--       1. Write a constructor for a parser: term1
+-- , which takes a Char and returns a Parser Char which will
+-- successfully parse the first character of the input if it is the character in question and will fail otherwise.
+-- 2. Write a constructor for a parser: digit which will parse one digit (0 − 9) from the input.
+-- 3. Write a constructor for a parser singularWhiteSpace which will parse a whitespace character (consider
+-- only the four whitespace characters: space, tab, carriage return, and linefeed) from the input.
+-- 4. Write a constructor for a parser endOfStream: which succeeds if the string is empty and fails otherwise.
--- test/Lambda/ParserTest.hs
+++ test/Lambda/ParserTest.hs
@@ -1,6 +1,6 @@
 module Lambda.ParserTest (parserTests) where
 
-import Parser (Parser (..), satisfy)
+import Parser (Parser (..), satisfy, term1)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
@@ -12,7 +12,7 @@ parserTests =
     [ testCase "satisfy matches character" $ parseA "abc" @?= Just ('a', "bc"),
       testCase "satisfy does not match character" $ parseA "xbc" @?= Nothing,
       testCase "satisfy on empty string" $ parseA "" @?= Nothing,
-      quickCheckAnagramTests
+      quickTests
     ]
   where
     parseA = runParser (satisfy (== 'a'))
@@ -20,13 +20,33 @@ parserTests =
 runParser :: Parser a -> String -> Maybe (a, String)
 runParser (Parser p) = p
 
-quickCheckAnagramTests :: TestTree
-quickCheckAnagramTests =
+quickTests :: TestTree
+quickTests =
   testGroup
     "QuickCheck"
-    [ testProperty "satisfy strictly matches character" prop_satisfiesMatchingChar
+    [ testProperty "satisfy strictly matches character" prop_satisfiesMatchingChar,
+      testProperty "satisfy rejects non-matching character" prop_nonMatchingChar,
+      testProperty "satisfy rejects empty string" prop_emptyString,
+      testProperty "term1 matches character" prop_term1,
+      testProperty "term1 rejects non-matching character" prop_term1_nothing
     ]
 
 prop_satisfiesMatchingChar :: Char -> String -> Property
 prop_satisfiesMatchingChar c s =
   runParser (satisfy (== c)) (c : s) === Just (c, s)
+
+prop_nonMatchingChar :: Char -> Char -> String -> Property
+prop_nonMatchingChar c1 c2 s =
+  c1 /= c2 ==> runParser (satisfy (== c1)) (c2 : s) === Nothing
+
+prop_emptyString :: Char -> Property
+prop_emptyString c =
+  runParser (satisfy (== c)) "" === Nothing
+
+prop_term1 :: Char -> String -> Property
+prop_term1 c s =
+  runParser (term1 c) (c : s) === Just (c, s)
+
+prop_term1_nothing :: Char -> Char -> String -> Property
+prop_term1_nothing c1 c2 s =
+  c1 /= c2 ==> runParser (term1 c1) (c2 : s) === Nothing
