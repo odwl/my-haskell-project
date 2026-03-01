efd4392e7aa7bb6134615dff66695631f9636e04
odwl <odewolf@gmail.com>
2026-03-01 11:13:32 +0100

    chore: refine parser and improve quickcheck properties for identifier

--- src/Lambda/Parser.hs
+++ src/Lambda/Parser.hs
@@ -1,62 +1,168 @@
-module Lambda.Parser (Parser (..), satisfy, parseChar, parseString, digit, whiteSpace, endOfStream, parserInt, parseTwoChars) where
+module Lambda.Parser (ReadP, digit, whiteSpace, endOfStream, parseTwoChars, parseDigits, identifier) where
 
-import Control.Applicative (Alternative (..))
-import Control.Monad (ap)
-import Data.Bifunctor (first)
-import Data.Char
+import Data.Char (isAlpha, isAlphaNum, isAscii, isDigit, isSpace)
+import Text.ParserCombinators.ReadP (ReadP, eof, munch, munch1, satisfy, string)
 
-newtype Parser a = Parser
-  { parse :: String -> Maybe (a, String)
-  }
-
-instance Functor Parser where
-  fmap f (Parser p) = Parser (fmap (first f) . p)
-
-instance Applicative Parser where
-  pure x = Parser (\s -> Just (x, s))
-  (<*>) = ap
-
-instance Alternative Parser where
-  empty = Parser (const Nothing)
-  (Parser p1) <|> (Parser p2) = Parser $ \s -> p1 s <|> p2 s
-
-instance Monad Parser where
-  (>>=) (Parser p) f = Parser $ \s -> case p s of
-    Nothing -> Nothing
-    Just (x, s') -> parse (f x) s'
-
-satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = Parser f
-  where
-    f (x : xs) | p x = Just (x, xs)
-    f _ = Nothing
-
-parseChar :: Char -> Parser Char
-parseChar c = satisfy (== c)
-
-parseString :: String -> Parser String
-parseString = traverse parseChar
-
-digit :: Parser Char
+digit :: ReadP Char
 digit = satisfy isDigit
 
-whiteSpace :: Parser Char
+whiteSpace :: ReadP Char
 whiteSpace = satisfy isSpace
 
-endOfStream :: Parser ()
-endOfStream = Parser f
-  where
-    f [] = Just ((), "")
-    f _ = Nothing
+endOfStream :: ReadP ()
+endOfStream = eof
 
-parserInt :: Parser Int
-parserInt = digitToInt <$> digit
+-- Specific parsers from the original file
 
-parseTwoChars :: Parser String
-parseTwoChars = parseString "ab"
+parseTwoChars :: ReadP String
+parseTwoChars = string "ab"
 
--- parseTwoChars = (\a b -> [a, b]) <$> parseChar 'a' <*> parseChar 'b'
--- parseTwoChars = do
---   c1 <- term1 'a'
---   c2 <- term1 'b'
---   return [c1, c2]
+parseDigits :: ReadP Int
+parseDigits = read <$> munch1 isDigit
+
+(.&&.) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+f .&&. g = \x -> f x && g x
+
+infixr 3 .&&.
+
+-- (.||.) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+-- f .||. g = \x -> f x || g x
+
+-- infixr 2 .||.
+
+identifier :: ReadP String
+identifier = do
+  c <- satisfy (isAscii .&&. isAlpha)
+  cs <- munch (isAscii .&&. isAlphaNum)
+  return (c : cs)
+
+-- -- ==========================================
+-- -- AST and ReadP Definitions
+-- -- ==========================================
+
+-- -- | Spaces parser
+-- spaces :: ReadP ()
+-- spaces = many whiteSpace *> pure ()
+
+-- -- | Token parser
+-- token :: ReadP a -> ReadP a
+-- token p = p <* spaces
+
+-- -- | Symbol parser
+-- symbol :: String -> ReadP String
+-- symbol s = token (parseString s)
+
+-- -- | Types
+-- type Id = String
+
+-- data Stmt
+--   = While Exp [Stmt]
+--   | Assign Id Exp
+--   deriving (Show, Eq)
+
+-- data Exp
+--   = If Exp Exp Exp
+--   | Cmp AExp CmpOp AExp
+--   | Not Exp
+--   | E_AExp AExp
+--   deriving (Show, Eq)
+
+-- data AExp
+--   = Num Int
+--   | Var Id
+--   | Op AExp BinOp AExp
+--   deriving (Show, Eq)
+
+-- data CmpOp = Le | Gt | Eq | Neq deriving (Show, Eq)
+
+-- data BinOp = Add | Sub | Mul | Div deriving (Show, Eq)
+
+-- -- | ReadPs
+
+-- parseStmts :: ReadP [Stmt]
+-- parseStmts = do
+--   s <- parseStmt
+--   (symbol ";" >> ((s :) <$> parseStmts)) <|> pure [s]
+
+-- parseStmt :: ReadP Stmt
+-- parseStmt = parseWhile <|> parseAssign
+
+-- parseWhile :: ReadP Stmt
+-- parseWhile = do
+--   symbol "while"
+--   cond <- parseExp
+--   symbol "do"
+--   body <- parseStmts
+--   symbol "done"
+--   return (While cond body)
+
+-- parseAssign :: ReadP Stmt
+-- parseAssign = do
+--   v <- identifier
+--   symbol ":="
+--   e <- parseExp
+--   return (Assign v e)
+
+-- parseExp :: ReadP Exp
+-- parseExp = parseIf <|> parseNot <|> parseCmpOrAExp
+
+-- parseIf :: ReadP Exp
+-- parseIf = do
+--   symbol "if"
+--   c <- parseExp
+--   symbol "then"
+--   t <- parseExp
+--   symbol "else"
+--   e <- parseExp
+--   symbol "fi"
+--   return (If c t e)
+
+-- parseCmpOrAExp :: ReadP Exp
+-- parseCmpOrAExp = do
+--   a1 <- parseAExp
+--   (do
+--      op <- parserCmpOp
+--      a2 <- parseAExp
+--      return (Cmp a1 op a2)
+--    ) <|> return (E_AExp a1)
+
+-- parseNot :: ReadP Exp
+-- parseNot = do
+--   symbol "not"
+--   Not <$> parseExp
+
+-- parseAExp :: ReadP AExp
+-- parseAExp = parseNum <|> parseVar <|> parseBinOp
+
+-- parseNum :: ReadP AExp
+-- parseNum = Num . read <$> token (some digit)
+
+-- parseVar :: ReadP AExp
+-- parseVar = Var <$> identifier
+
+-- parseBinOp :: ReadP AExp
+-- parseBinOp = do
+--   symbol "("
+--   a1 <- parseAExp
+--   op <- parserBinOpSym
+--   a2 <- parseAExp
+--   symbol ")"
+--   return (Op a1 op a2)
+
+-- parserCmpOp :: ReadP CmpOp
+-- parserCmpOp = (symbol "<=" *> pure Le)
+--           <|> (symbol ">" *> pure Gt)
+--           <|> (symbol "==" *> pure Eq)
+--           <|> (symbol "!=" *> pure Neq)
+
+-- parserBinOpSym :: ReadP BinOp
+-- parserBinOpSym = (symbol "+" *> pure Add)
+--              <|> (symbol "-" *> pure Sub)
+--              <|> (symbol "*" *> pure Mul)
+--              <|> (symbol "/" *> pure Div)
+
+-- identifier :: ReadP String
+-- identifier = token $ do
+--   c <- satisfy (\c -> isAscii c && isAlpha c)
+--   cs <- many (satisfy (\c -> isAscii c && isAlphaNum c))
+--   return (c:cs)
--- test/Lambda/ParserTest.hs
+++ test/Lambda/ParserTest.hs
@@ -3,13 +3,20 @@
 module Lambda.ParserTest (parserTests) where
 
 import Control.Applicative (Alternative (..))
-import Data.Char (isDigit, isSpace)
-import Lambda.Parser (Parser (..), digit, endOfStream, parseChar, parseString, parseTwoChars, parserInt, satisfy, whiteSpace)
+import Data.Char (isAlpha, isAlphaNum, isAscii, isDigit, isSpace)
+import Lambda.Parser (ReadP, digit, endOfStream, identifier, parseDigits, parseTwoChars, whiteSpace)
+import Text.ParserCombinators.ReadP (char, pfail, readP_to_S, satisfy, string)
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes (applicative, functor, monad)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
+
+-- | Run parser and return the first result
+runReadP :: ReadP a -> String -> Maybe (a, String)
+runReadP p s = case readP_to_S p s of
+  [] -> Nothing
+  ((x, s') : _) -> Just (x, s') 
 
 parserTests :: TestTree
 parserTests =
@@ -18,23 +25,20 @@ parserTests =
     [ testCase "satisfy matches character" $ parseA "abc" @?= Just ('a', "bc"),
       testCase "satisfy does not match character" $ parseA "xbc" @?= Nothing,
       testCase "satisfy on empty string" $ parseA "" @?= Nothing,
-      testCase "endOfStream matches empty string" $ runParser endOfStream "" @?= Just ((), ""),
-      testCase "fmap maps Parser Char to Parser String" $ runParser ((: []) <$> satisfy (== 'a')) "abc" @?= Just ("a", "bc"),
-      testCase "fmap maps with custom lambda" $ runParser ((:) <*> (: []) <$> satisfy (== 'a')) "abc" @?= Just ("aa", "bc"),
+      testCase "endOfStream matches empty string" $ runReadP endOfStream "" @?= Just ((), ""),
+      testCase "fmap maps ReadP Char to ReadP String" $ runReadP ((: []) <$> satisfy (== 'a')) "abc" @?= Just ("a", "bc"),
+      testCase "fmap maps with custom lambda" $ runReadP ((:) <*> (: []) <$> satisfy (== 'a')) "abc" @?= Just ("aa", "bc"),
       quickTests,
       tastyBatch
-        (functor (undefined :: Parser (Int, String, Int))),
+        (functor (undefined :: ReadP (Int, String, Int))),
       tastyBatch
-        (applicative (undefined :: Parser (Int, String, Int))),
+        (applicative (undefined :: ReadP (Int, String, Int))),
       tastyBatch
-        (monad (undefined :: Parser (Int, String, Int)))
+        (monad (undefined :: ReadP (Int, String, Int)))
     ]
   where
-    parseA = runParser (satisfy (== 'a'))
+    parseA = runReadP (satisfy (== 'a'))
     tastyBatch (name, tests) = testProperties name tests
-
-runParser :: Parser a -> String -> Maybe (a, String)
-runParser (Parser p) = p
 
 quickTests :: TestTree
 quickTests =
@@ -43,9 +47,6 @@ quickTests =
     [ testProperty "satisfy strictly matches character" prop_satisfiesMatchingChar,
       testProperty "satisfy rejects non-matching character" prop_nonMatchingChar,
       testProperty "satisfy rejects empty string" prop_emptyString,
-      testProperty "parseChar matches character" prop_parseChar,
-      testProperty "parseChar rejects non-matching character" prop_parseChar_nothing,
-      testProperty "parseString matches specific string" prop_parseString,
       testProperty "Alternative <|> tries second parser on failure" prop_alternative_choice,
       testProperty "Alternative empty always fails" prop_alternative_empty,
       testProperty "Alternative Law: Left Identity" prop_alternative_left_identity,
@@ -56,114 +57,130 @@ quickTests =
       testProperty "whiteSpace matches whitespace" prop_whiteSpace,
       testProperty "whiteSpace rejects non-whitespace" prop_not_whiteSpace,
       testProperty "endOfStream rejects non-empty string" prop_endOfStream_nonEmpty,
-      testProperty "parserInt parses a single digit correctly" prop_parserInt_digit,
-      testProperty "parserInt rejects non-digit structures" prop_parserInt_not_digit,
       testProperty "parseTwoChars matches both chars" prop_parseTwoChars_match_both,
       testProperty "parseTwoChars matches first char only" prop_parseTwoChars_match_first_only,
       testProperty "parseTwoChars matches second char only" prop_parseTwoChars_match_second_only,
-      testProperty "parseTwoChars matches none" prop_parseTwoChars_match_none
+      testProperty "parseTwoChars matches none" prop_parseTwoChars_match_none,
+      testProperty "parseDigits parses sequence of digits" prop_parseDigits,
+      testProperty "identifier parses valid names" prop_identifier_valid,
+      testProperty "identifier rejects names with invalid start" prop_identifier_reject_invalid_start
     ]
 
-instance (Arbitrary a) => Arbitrary (Parser a) where
+instance (Arbitrary a) => Arbitrary (ReadP a) where
   arbitrary = do
     b <- arbitrary
-    -- Generate a parser that either succeeds with a value and arbitrary leftover string, or fails
+    -- Generate a parser that either succeeds with a value and arbitrary leftover string (pure), or fails
     elements
-      [ Parser (const Nothing),
-        Parser (\s -> Just (b, s))
+      [ pfail,
+        pure b
       ]
 
-instance (Show a) => Show (Parser a) where
+instance (Show a) => Show (ReadP a) where
   show _ = "<Parser>"
 
-instance (Eq a, Show a) => EqProp (Parser a) where
+instance (Eq a, Show a) => EqProp (ReadP a) where
   p1 =-= p2 =
-    property $ \s -> runParser p1 s === runParser p2 s
+    property $ \s -> runReadP p1 s === runReadP p2 s
 
 prop_satisfiesMatchingChar :: Char -> String -> Property
 prop_satisfiesMatchingChar c s =
-  runParser (satisfy (== c)) (c : s) === Just (c, s)
+  runReadP (satisfy (== c)) (c : s) === Just (c, s)
 
 prop_nonMatchingChar :: Char -> Char -> String -> Property
 prop_nonMatchingChar c1 c2 s =
-  c1 /= c2 ==> runParser (satisfy (== c1)) (c2 : s) === Nothing
+  c1 /= c2 ==> runReadP (satisfy (== c1)) (c2 : s) === Nothing
 
 prop_emptyString :: Char -> Property
 prop_emptyString c =
-  runParser (satisfy (== c)) "" === Nothing
-
-prop_parseChar :: Char -> String -> Property
-prop_parseChar c s =
-  runParser (parseChar c) (c : s) === Just (c, s)
-
-prop_parseChar_nothing :: Char -> Char -> String -> Property
-prop_parseChar_nothing c1 c2 s =
-  c1 /= c2 ==> runParser (parseChar c1) (c2 : s) === Nothing
-
-prop_parseString :: String -> String -> Property
-prop_parseString target rest =
-  runParser (parseString target) (target ++ rest) === Just (target, rest)
+  runReadP (satisfy (== c)) "" === Nothing
 
 prop_alternative_choice :: String -> Property
 prop_alternative_choice s =
-  runParser (parseChar 'a' <|> parseChar 'b') ("b" ++ s) === Just ('b', s)
+  runReadP (char 'a' <|> char 'b') ("b" ++ s) === Just ('b', s)
 
 prop_alternative_empty :: String -> Property
 prop_alternative_empty s =
-  runParser (empty :: Parser Int) s === Nothing
+  runReadP (empty :: ReadP Int) s === Nothing
 
-prop_alternative_left_identity :: Parser Int -> String -> Property
+prop_alternative_left_identity :: ReadP Int -> String -> Property
 prop_alternative_left_identity p s =
-  runParser (empty <|> p) s === runParser p s
+  runReadP (empty <|> p) s === runReadP p s
 
-prop_alternative_right_identity :: Parser Int -> String -> Property
+prop_alternative_right_identity :: ReadP Int -> String -> Property
 prop_alternative_right_identity p s =
-  runParser (p <|> empty) s === runParser p s
+  runReadP (p <|> empty) s === runReadP p s
 
 prop_alternative_left_bias :: String -> Property
 prop_alternative_left_bias s =
-  runParser (parseString "a" <|> parseString "ab") ("ab" ++ s) === Just ("a", "b" ++ s)
+  runReadP (string "a" <|> string "ab") ("ab" ++ s) === Just ("a", "b" ++ s)
 
 prop_digit :: String -> Property
 prop_digit s = forAll (elements ['0' .. '9']) $ \c ->
-  runParser digit (c : s) === Just (c, s)
+  runReadP digit (c : s) === Just (c, s)
 
 prop_not_digit :: String -> Property
 prop_not_digit s = forAll (arbitrary `suchThat` (not . isDigit)) $ \c ->
-  runParser digit (c : s) === Nothing
+  runReadP digit (c : s) === Nothing
 
 prop_whiteSpace :: String -> Property
 prop_whiteSpace s = forAll (elements " \t\n\r\f\v") $ \c ->
-  runParser whiteSpace (c : s) === Just (c, s)
+  runReadP whiteSpace (c : s) === Just (c, s)
 
 prop_not_whiteSpace :: String -> Property
 prop_not_whiteSpace s = forAll (arbitrary `suchThat` (not . isSpace)) $ \c ->
-  runParser whiteSpace (c : s) === Nothing
+  runReadP whiteSpace (c : s) === Nothing
 
 prop_endOfStream_nonEmpty :: Char -> String -> Property
 prop_endOfStream_nonEmpty c s =
-  runParser endOfStream (c : s) === Nothing
+  runReadP endOfStream (c : s) === Nothing
 
-prop_parserInt_digit :: String -> Property
-prop_parserInt_digit s = forAll (elements ['0' .. '9']) $ \c ->
-  runParser parserInt (c : s) === Just (read [c], s)
 
-prop_parserInt_not_digit :: Char -> String -> Property
-prop_parserInt_not_digit c s =
-  not (isDigit c) ==> runParser parserInt (c : s) === Nothing
 
 prop_parseTwoChars_match_both :: String -> Property
 prop_parseTwoChars_match_both s =
-  runParser parseTwoChars ("ab" ++ s) === Just ("ab", s)
+  runReadP parseTwoChars ("ab" ++ s) === Just ("ab", s)
 
 prop_parseTwoChars_match_first_only :: Char -> String -> Property
 prop_parseTwoChars_match_first_only c s =
-  c /= 'b' ==> runParser parseTwoChars ('a' : c : s) === Nothing
+  c /= 'b' ==> runReadP parseTwoChars ('a' : c : s) === Nothing
 
 prop_parseTwoChars_match_second_only :: Char -> String -> Property
 prop_parseTwoChars_match_second_only c s =
-  c /= 'a' ==> runParser parseTwoChars (c : 'b' : s) === Nothing
+  c /= 'a' ==> runReadP parseTwoChars (c : 'b' : s) === Nothing
 
 prop_parseTwoChars_match_none :: Char -> Char -> String -> Property
 prop_parseTwoChars_match_none c1 c2 s =
-  c1 /= 'a' && c2 /= 'b' ==> runParser parseTwoChars (c1 : c2 : s) === Nothing
+  c1 /= 'a' && c2 /= 'b' ==> runReadP parseTwoChars (c1 : c2 : s) === Nothing
+
+prop_parseDigits :: NonNegative Int -> Char -> String -> Property
+prop_parseDigits (NonNegative n) c s = not (isDigit c) ==> 
+  runReadP parseDigits (show n ++ (c:s)) === Just (n, c:s)
+
+(&&&) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+f &&& g = \x -> f x && g x
+infixr 3 &&&
+
+genInputStartingWithId :: Gen (String, String)
+genInputStartingWithId = do
+  idFirst <- arbitrary `suchThat` (isAscii &&& isAlpha)
+  idRest <- listOf $ arbitrary `suchThat` (isAscii &&& isAlphaNum)
+  separator <- arbitrary `suchThat` (\c -> not (isAscii c && isAlphaNum c))
+  rest <- arbitrary
+  return (idFirst : idRest, separator : rest)
+
+prop_identifier_valid :: Property
+prop_identifier_valid = 
+  forAll genInputStartingWithId $ \(idStr, rest) ->
+    runReadP identifier (idStr ++ rest) === Just (idStr, rest)
+
+genInvalidStartChar :: Gen Char
+genInvalidStartChar = frequency
+  [ (1, elements ['0'..'9'])
+  , (3, arbitrary `suchThat` (\c -> not (isAscii c && isAlpha c)))
+  ]
+
+prop_identifier_reject_invalid_start :: String -> Property
+prop_identifier_reject_invalid_start s = 
+  forAll genInvalidStartChar $ \c ->
+    classify (isDigit c) "first char is digit" $
+      runReadP identifier (c : s) === Nothing
