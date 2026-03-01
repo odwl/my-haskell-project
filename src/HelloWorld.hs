d3b845ab14da87e4b4086faffa45a2584fb69e91
odwl <odewolf@gmail.com>
2026-03-01 14:13:21 +0100

    feat: Implement sequential statement parsing in Lambda.Parser and remove the HelloWorld module and its associated tests.

--- src/HelloWorld.hs
+++
@@ -1,8 +0,0 @@
-module HelloWorld where
-
-sayHello :: String -> String
-sayHello name = "Hello, " ++ name ++ "!"
-
--- Optional: A main function if you want to run this file directly
-main :: IO ()
-main = putStrLn (sayHello "World")
