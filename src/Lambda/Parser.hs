f153e9bd63446461b73d5a6df8dc3582d50b85e8
odwl <odewolf@gmail.com>
2026-03-02 15:49:09 +0100

    refactor: Reorganize type definitions, remove `unId`, and improve parser implementations using `guard`, `choice`, and `sepBy1`.

--- src/Lambda/Parser.hs
+++ src/Lambda/Parser.hs
@@ -1,33 +1,78 @@
-module Lambda.Parser (ReadP, Id, mkId, unId, num, binop, cmpop, identifier, aexp, expr, stmt, stmts, Stmt (..), Exp (..), AExp (..), BinOp (..), CmpOp (..)) where
+module Lambda.Parser (ReadP, Id, mkId, num, binop, cmpop, identifier, aexp, expr, stmt, stmts, Stmt (..), Exp (..), AExp (..), BinOp (..), CmpOp (..)) where
 
+import Control.Monad (guard, unless)
 import Data.Char (isAlpha, isAlphaNum, isAscii, isDigit)
-import Text.ParserCombinators.ReadP (ReadP, char, munch, munch1, pfail, satisfy, skipSpaces, string, (<++))
+import Text.ParserCombinators.ReadP (ReadP, char, choice, munch, munch1, pfail, satisfy, sepBy1, skipSpaces, string, (<++))
+
+-- ==========================================
+-- Types Definitions
+-- ==========================================
+
+data Id = Id Char String
+  deriving (Show, Eq)
+
+-- | Smart constructor that explicitly takes the starting Char and the rest of the String.
+mkId :: Char -> String -> Maybe Id
+mkId c cs
+  | isAscii c && isAlpha c && all (\x -> isAscii x && isAlphaNum x) cs = Just (Id c cs)
+  | otherwise = Nothing
+
+data CmpOp = Le | Gt | Eq | Neq deriving (Show, Eq)
+
+data BinOp = Add | Sub | Mul | Div deriving (Show, Eq)
+
+data AExp
+  = Num Int
+  | Var Id
+  | Op AExp BinOp AExp
+  deriving (Show, Eq)
+
+data Exp
+  = If Exp Exp Exp
+  | Cmp AExp CmpOp AExp
+  | Not Exp
+  | E_AExp AExp
+  deriving (Show, Eq)
+
+data Stmt
+  = While Exp [Stmt]
+  | Assign Id Exp
+  deriving (Show, Eq)
+
+-- ==========================================
+-- Parser Definitions
+-- ==========================================
 
 identifier :: ReadP Id
 identifier = lexeme $ do
   c <- satisfy (liftA2 (&&) isAscii isAlpha)
   cs <- munch (liftA2 (&&) isAscii isAlphaNum)
   let name = c : cs
-  if name `elem` ["not", "if", "then", "else", "fi", "while", "do", "done"] then pfail else return (Id c cs)
-
-num :: ReadP Int
-num = lexeme $ read <$> munch1 isDigit
+  guard (name `notElem` ["not", "if", "then", "else", "fi", "while", "do", "done"])
+  return (Id c cs)
 
 binop :: ReadP BinOp
 binop =
   lexeme $
-    (Add <$ char '+')
-      <++ (Sub <$ char '-')
-      <++ (Mul <$ char '*')
-      <++ (Div <$ char '/')
+    choice
+      [ Add <$ char '+',
+        Sub <$ char '-',
+        Mul <$ char '*',
+        Div <$ char '/'
+      ]
 
 cmpop :: ReadP CmpOp
 cmpop =
   lexeme $
-    (Le <$ string "<=")
-      <++ (Gt <$ char '>')
-      <++ (Eq <$ string "==")
-      <++ (Neq <$ string "!=")
+    choice
+      [ Le <$ string "<=",
+        Gt <$ char '>',
+        Eq <$ string "==",
+        Neq <$ string "!="
+      ]
+
+num :: ReadP Int
+num = lexeme $ read <$> munch1 isDigit
 
 aexp :: ReadP AExp
 aexp = (Num <$> num) <++ (Var <$> identifier) <++ opP
@@ -74,9 +119,7 @@ stmt = assignP <++ whileP
         <* keyword "done"
 
 stmts :: ReadP [Stmt]
-stmts = do
-  s <- stmt
-  (lexeme (string ";") *> ((s :) <$> stmts)) <++ return [s]
+stmts = sepBy1 stmt (lexeme (string ";"))
 
 -- -- ==========================================
 -- -- AST and ReadP Definitions
@@ -90,39 +133,4 @@ lexeme p = p <* skipSpaces
 keyword :: String -> ReadP ()
 keyword k = lexeme $ do
   kw <- munch1 (liftA2 (&&) isAscii isAlphaNum)
-  if kw == k then return () else pfail
-
--- | Types
-data Id = Id Char String
-  deriving (Show, Eq)
-
-unId :: Id -> String
-unId (Id c cs) = c : cs
-
--- | Smart constructor that explicitly takes the starting Char and the rest of the String.
-mkId :: Char -> String -> Maybe Id
-mkId c cs
-  | isAscii c && isAlpha c && all (\x -> isAscii x && isAlphaNum x) cs = Just (Id c cs)
-  | otherwise = Nothing
-
-data Stmt
-  = While Exp [Stmt]
-  | Assign Id Exp
-  deriving (Show, Eq)
-
-data Exp
-  = If Exp Exp Exp
-  | Cmp AExp CmpOp AExp
-  | Not Exp
-  | E_AExp AExp
-  deriving (Show, Eq)
-
-data AExp
-  = Num Int
-  | Var Id
-  | Op AExp BinOp AExp
-  deriving (Show, Eq)
-
-data CmpOp = Le | Gt | Eq | Neq deriving (Show, Eq)
-
-data BinOp = Add | Sub | Mul | Div deriving (Show, Eq)
+  unless (kw == k) pfail
