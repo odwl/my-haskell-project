b64abb5f38bab136eb8788eaa0f19d4c4be884f7
Olivier De Wolf <odwl@google.com>
2026-06-08 06:36:02 +0000

    Use ++ notation for string concatenation in Lean theorems

--- lean/Main.lean
+++ lean/Main.lean
@@ -6,13 +6,13 @@ Reference: https://anggtwu.net/2023.1-LA/fplean4-inicio.pdf
 -- 42 + 19
 theorem myAdd_42_19 : Nat.add 42 19 = 61 := rfl
 
--- String.append "A" (String.append "B" "C")
-theorem stringConcat_AB_C : String.append "A" (String.append "B" "C") = "ABC" := rfl
+-- "A" ++ ("B" ++ "C")
+theorem stringConcat_AB_C : "A" ++ ("B" ++ "C") = "ABC" := rfl
 
--- String.append (String.append "A" "B") "C"
-theorem stringConcat_A_BC : String.append (String.append "A" "B") "C" = "ABC" := rfl
+-- ("A" ++ "B") ++ "C"
+theorem stringConcat_A_BC : ("A" ++ "B") ++ "C" = "ABC" := rfl
 
-theorem concatAssociative (a b c : String) : String.append (String.append a b) c = String.append a (String.append b c) :=
+theorem concatAssociative (a b c : String) : (a ++ b) ++ c = a ++ (b ++ c) :=
   String.append_assoc
 
 
