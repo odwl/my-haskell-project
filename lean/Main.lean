05ffba5559e07cc41a7fe1e26400d7e9cc760074
Olivier De Wolf <odwl@google.com>
2026-06-08 07:50:02 +0000

    Verify joinStringsWith exercises and refactor proofs to examples

--- lean/Main.lean
+++ lean/Main.lean
@@ -18,17 +18,15 @@ theorem concatAssociative (a b c : String) : (a ++ b) ++ c = a ++ (b ++ c) :=
 -- if 3 == 3 then 5 else 7
 theorem if3eq3 : (if 3 == 3 then 5 else 7) = 5 := rfl
 
+example (a b c: Nat) : (if a == a then b else c) = b := by simp
+
 -- joinStringWith
-def joinStringWith (sep a b: String) : String := a ++ sep ++ b
+def joinStringsWith (sep a b: String) : String := a ++ sep ++ b
+#guard joinStringsWith ", " "one" "another" == "one, another"
 
-theorem joinComma : joinStringWith ", " "one" "another" = "one, another" := rfl
+example : String -> String -> String := joinStringsWith ": "
 
-#guard joinStringWith ", " "one" "another" == "one, another"
-
-
-
--- if 3 == 4 then "equal" else "not equal"
-theorem ifaeqaplus1 (a b c: Nat) : (if a == a +1 then b else c ) = c := by simp
+example (a b c: Nat) : (if a == a +1 then b else c ) = c := by simp
 
 def add1 (n : Nat) : Nat := n + 1
 
